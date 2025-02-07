acda614fd7606d632f30333c8432deb65efcb53c
Miroslav Saracevic <dev.msaracevic@gmail.com>
2025-02-08 00:03:15 +0100

    Rewrite the code to be in this decade

--- src/ReactEmbedGist.js
+++ src/ReactEmbedGist.js
@@ -1,124 +1,93 @@
-import React, { Component } from "react";
-import { v4 as uuid } from "uuid";
+import React, {useCallback, useEffect, useState} from 'react';
+import {v4 as uuid} from 'uuid';
 
-export default class ReactEmbedGist extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { loading: true, title: "", content: "" };
-    this.handleNetworkErrors = this.handleNetworkErrors.bind(this);
-  }
+const ReactEmbedGist = ({
+  gist,
+  file,
+  loadingClass,
+  wrapperClass,
+  titleClass,
+  contentClass,
+  errorClass,
+  loadingFallback,
+}) => {
+  const [loading, setLoading] = useState(true);
+  const [title, setTitle] = useState(null);
+  const [content, setContent] = useState(null);
+  const [error, setError] = useState(null);
 
-  componentDidMount() {
-    this.getGist();
-  }
+  // Unfortunately there is no clean / easy way to track if this is 404 or something else
+  // In that case just say it failed to load regardless of reason
+  const handleNetworkErrors = useCallback(() => {
+    setError(`${gist} failed to load`);
+    setLoading(false);
+  }, [gist]);
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (prevProps.gist !== this.props.gist) this.getGist();
-  }
-
-  async getGist() {
-    /*
-     * Load gist from GitHub and attach callback to be executed once this script finishes loading
-     * The callbacks are going to be named as gist_callback_:instanceId where instanceId is random UUID
-     */
-    const { gist, file } = this.props;
-
-    /*
-     * Check if the response is fine and has ID assigned from Gist
-     */
-    if (!gist.split("/")[1])
-      return this.setState({
-        loading: false,
-        error: `${gist} is not valid format`,
-      });
-
-    const instanceId = uuid().replace(/-/g, "");
-    await this.setState({ loading: true });
-    this.setupCallback(instanceId);
-
-    const script = document.createElement("script");
-    let url = `https://gist.github.com/${gist}.json?callback=gist_callback_${instanceId}`;
-    if (file) url += `&file=${file}`;
-    script.type = "text/javascript";
-    script.src = url;
-    script.onerror = (e) => this.handleNetworkErrors(e);
-    document.head.appendChild(script);
-  }
-
-  handleNetworkErrors(e) {
-    /*
-     * Unfortunately there is no clean / easy way to track if this is 404 or something else, so in that case
-     * just say it failed to load regardless of reason
-     */
-    this.setState({
-      loading: false,
-      error: `${this.props.gist} failed to load`,
-    });
-  }
-
-  setupCallback(instanceId) {
+  // Once we call this callback, we are going to set description of gist as title and fill the content.
+  // We are also going to set loading flag into false to render the content
+  const setupCallback = (instanceId) => {
     window[`gist_callback_${instanceId}`] = function (gist) {
-      /*
-       * Once we call this callback, we are going to set description of gist as title and fill the content. We are
-       * also going to set loading flag into false to render the content
-       */
-      const nextState = { loading: false, error: gist.error || null };
+      setLoading(false);
 
-      if (!nextState.error) {
-        nextState.title = gist.description;
-        nextState.content = `${gist.div.replace(/href=/g, 'target="_blank" href=')}`;
-      }
+      if (gist.error) return setError(gist.error);
 
-      this.setState(nextState);
-
+      setTitle(gist.description);
+      setContent(`${gist.div.replace(/href=/g, 'target="_blank" href=')}`);
       /*
        * Not perfect way to do things, ideally parent component would have state of all loaded stylesheets
-       * and add them as needed, but because I want to keep this as module and you can call more than once
+       * and add them as needed, but because I want to keep this as module, and you can call more than once
        * this component, the only easy way for now is to check if the head already has this stylesheet inside
        *
        * NOTE: document.styleSheets doesn't work here, since stylesheet will be added there only once it is
        *       fully loaded, which means that if you have this component twice or more in a row there is high
-       *       chance it will not recognize this stylesheet as already added. Hence this solution with checking
+       *       chance it will not recognize this stylesheet as already added. Hence, this solution with checking
        *       innerHMTL to be sure that we don't have any css loaded twice
        */
       if (document.head.innerHTML.indexOf(gist.stylesheet) === -1) {
-        let stylesheet = document.createElement("link");
-        stylesheet.type = "text/css";
-        stylesheet.rel = "stylesheet";
+        let stylesheet = document.createElement('link');
+        stylesheet.type = 'text/css';
+        stylesheet.rel = 'stylesheet';
         stylesheet.href = gist.stylesheet;
         document.head.appendChild(stylesheet);
       }
-    }.bind(this);
-  }
+    };
+  };
 
-  render() {
-    const {
-      loadingClass,
-      wrapperClass,
-      titleClass,
-      contentClass,
-      errorClass,
-      loadingFallback,
-    } = this.props;
+  // Load gist from GitHub and attach callback to be executed once this script finishes loading
+  // The callbacks are going to be named as gist_callback_:instanceId where instanceId is random UUID
+  const getGist = useCallback(async () => {
+    // Check if the response is fine and has ID assigned from Gist
+    if (!gist.split('/')[1]) return setError(`${gist} is not valid format`);
 
-    if (this.state.loading) {
-      return (
-        <article className={loadingClass}>
-          {loadingFallback ? loadingFallback : "Loading ..."}
-        </article>
-      );
-    } else if (this.state.error) {
-      return <article className={errorClass}>{this.state.error}</article>;
-    } else {
-      return (
-        <article className={wrapperClass}>
-          <h2 className={titleClass}>{this.state.title}</h2>
-          <section
-            className={contentClass}
-            dangerouslySetInnerHTML={{ __html: this.state.content }}
-          />
-        </article>
-      );
-    }
+    const instanceId = uuid().replace(/-/g, '');
+    setLoading(true);
+    setupCallback(instanceId);
+
+    const script = document.createElement('script');
+    let url = `https://gist.github.com/${gist}.json?callback=gist_callback_${instanceId}`;
+    if (file) url += `&file=${file}`;
+    script.type = 'text/javascript';
+    script.src = url;
+    script.onerror = () => handleNetworkErrors();
+    document.head.appendChild(script);
+  }, [file, gist, handleNetworkErrors]);
+
+  useEffect(() => {
+    getGist().then(() => setLoading(false));
+  }, [gist, file, getGist]);
+
+  if (loading) {
+    return <article className={loadingClass}>{loadingFallback ? loadingFallback : 'Loading ...'}</article>;
+  } else if (error) {
+    return <article className={errorClass}>{error}</article>;
+  } else if (title && content) {
+    return (
+      <article className={wrapperClass}>
+        <h2 className={titleClass}>{title}</h2>
+        <section className={contentClass} dangerouslySetInnerHTML={{__html: content}}/>
+      </article>
+    );
   }
-}
+};
+
+export default ReactEmbedGist;
--- src/ReactEmbedGist.test.js
+++ src/ReactEmbedGist.test.js
@@ -1,22 +1,24 @@
 import React from "react";
-import renderer from 'react-test-renderer';
+import renderer, {act} from 'react-test-renderer';
 
 import ReactEmbedGist from "./ReactEmbedGist";
 
 test("handles loading fallback", async () => {
-    const testRenderer = renderer.create(
-        <ReactEmbedGist
-            gist="msaracevic/5d757e2fc72482a9a4a439969500c2eb"
-            wrapperClass="gist__bash"
-            loadingClass="loading__screen"
-            titleClass="gist__title"
-            errorClass="gist__error"
-            contentClass="gist__content"
-            file=".bash_profile.sh"
-            loadingFallback={<p>Loading fallback</p>}
-        />
-    );
+  let testRenderer;
 
-    const testInstance = testRenderer.root;
-    expect(testInstance.findByProps({className: "loading__screen"}).children.length).toBe(1)
+  act(() => testRenderer = renderer.create(
+    <ReactEmbedGist
+      gist="msaracevic/5d757e2fc72482a9a4a439969500c2eb"
+      wrapperClass="gist__bash"
+      loadingClass="loading__screen"
+      titleClass="gist__title"
+      errorClass="gist__error"
+      contentClass="gist__content"
+      file=".bash_profile.sh"
+      loadingFallback={<p>Loading fallback</p>}
+    />
+  ));
+
+  const testInstance = testRenderer.root;
+  expect(testInstance.findByProps({className: "loading__screen"}).children.length).toBe(1)
 });
